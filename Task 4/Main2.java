90a15cb1e5cb25c89eec8732b567df915ed894b9
fahima-999 <fahimafahi999@gmail.com>
2026-06-23 10:57:05 +0600

    Implement Employee class with bonus calculation

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,58 @@
 
+public class Main{
+    public static void main(String[] args){
+    
+
+
+BonusEligible employee = new Employee("Alice");
+employee.calculateBonus();
+
+}
+}
+
+ 
+ abstract class TeamMember{
+    protected String name;
+     
+     public TeamMember(String name ){
+     this.name = name;
+}
+
+public String getName(){
+    return name;
+}
+
+}
+
+
+interface BonusEligible{
+    void calculateBonus();
+}
+
+
+
+class Employee extends TeamMember implements BonusEligible{
+    public Employee(String name)
+{
+    super(name);
+} 
+
+@Override
+public void calculateBonus(){
+    System.out.println("Calculating standard employee bonus...");
+}
+    
+}
+
+ class Contractor extends TeamMember{
+    public Contractor(String name){
+        super(name);
+    }
+}
+
+
+
+
+
+
+
