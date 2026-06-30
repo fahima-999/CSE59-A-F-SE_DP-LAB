6beca6eb644ecfeae1dfd4aa3f89589d1d8170be
fahima-999 <fahimafahi999@gmail.com>
2026-06-30 11:05:21 +0600

    Implement quantization for ResNet18 and MobileNet

--- Task 5/Main4.java
+++ Task 5/Main4.java
@@ -1,1 +1,38 @@
+interface OptimizableModel{
+    void quantize();
+}
 
+class ResNet18 implements OptimizableModel{
+    @Override 
+    public void quantize(){
+        System.out.println("Applying int8 quantization for ResNet-18");
+    }
+}
+
+class MobileNet implements OptimizableModel{
+    @Override
+    public void quantize(){
+        System.out.println("Applyting dynamic quantization for MobileNet:");
+    }
+}
+
+
+class InferenceEngine{
+    public void optimizeModel(OptimizableModel model){
+        model.quantize();
+    }
+}
+
+public class Main4{
+    public static void main(String[] args) {
+        
+
+        InferenceEngine engine = new InferenceEngine();
+        OptimizableModel resnet = new ResNet18();
+
+        OptimizableModel mobilenet = new MobileNet();
+
+        engine.optimizeModel(resnet);
+        engine.optimizeModel(mobilenet);
+    }
+}
